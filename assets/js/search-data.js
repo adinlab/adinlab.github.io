8cb1ad6d1bb299b2c4cea24a8ac398d01a14f0a5
melihkandemir <melihkandemir@gmail.com>
2026-09-10 13:31:59 +0000

    Deploying to gh-pages from @ adinlab/adinlab.github.io@11305fd4af06ea89c366ba6d6eaa915eb5af8537 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-code",
-          title: "Code",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/code/";
-          },
         },{id: "nav-publications",
           title: "Publications",
           description: "",
